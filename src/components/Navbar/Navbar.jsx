95e8603961c6dbc3803642400ec1f6b6141ab440
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-07-28 15:58:22 +0100

    Setting up products and navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import ToolBar from "@material-ui/core/Toolbar";
+import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/images/shop-logo.png";
+import useStyles from "./styles";
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <ToolBar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Fakey-Commerce"
+              height="25px"
+              className={classes.image}
+            />
+            Fakey-Commerce
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </ToolBar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
